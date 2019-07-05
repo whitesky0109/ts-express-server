703b066c933e50a4a9ef0e328009ed3fb4a41584
Gunwoo Cho <gunwoo@hancommds.com>
2019-07-06 00:00:35 +0900

    remove polyfills.ts

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,5 +1,4 @@
 import 'reflect-metadata';
-import './polyfills';
 
 import { useContainer, useExpressServer } from 'routing-controllers';
 import { useContainer as useContainerSocket, useSocketServer } from 'socket-controllers';
--- src/server/polyfills.ts
+++
@@ -1,2 +0,0 @@
-/** reflect-metadata **/
-import 'reflect-metadata';
